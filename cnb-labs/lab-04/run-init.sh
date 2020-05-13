e02820a6eabb79a41ad665187c52c2e978f97e33
Grigorij <grigorij.k@gmail.com>
2020-05-14 00:25:20 +0300

    fix

--- cnb-labs/lab-04/run-init.sh
+++ cnb-labs/lab-04/run-init.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 git clone https://github.com/grigorijk/shipping-service-js
-mv shipping-service-js shipping-service && cd shipping-service
+mv shipping-service-js/ shipping-service && cd shipping-service
 rm -rf Dockerfile *.yml *.sh .git *.yaml .gitignore .s2i
 launch.sh
